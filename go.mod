6f2d29512e85e8c451debf4d9ed264641f039d3d
funasedaisuke <dai.boy.human@gmail.com>
2022-08-23 18:21:12 +0900

    220823:syakyo:http server + context

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/funasedaisuke/go-web-application
 
 go 1.18
+
+require golang.org/x/sync v0.0.0-20220819030929-7fc1605a5dde
--- main.go
+++ main.go
@@ -1,20 +1,45 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
 	"net/http"
-	"os"
+
+	"golang.org/x/sync/errgroup"
 )
 
 func main(){
-	err := http.ListenAndServe(":18081",
-	http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
-		fmt.Fprintf(w,"hello!:%s",r.URL.Path[1:])
-	}),
-	)
-if err != nil{
-	fmt.Printf("failed to terminate server: %v",err)
-	os.Exit(1)
+	//context.Background()は空のコンテキストを生成する。 
+	//コンテキストを使う事で関数の外部からサーバーのプロセスを中断できる
+	if err := run(context.Background());err != nil{
+		log.Printf("failed  to terminate server:%v",err)
+	}
 }
 
+func run(ctx context.Context)error{
+	s := &http.Server{
+		Addr: ":18080",
+		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
+		fmt.Fprintf(w,"hello!:%s",r.URL.Path[1:])
+	}),
+	}
+	eg,ctx := errgroup.WithContext(ctx)
+	//別ゴールーチンでサーバーを立ち上げる
+	//errgroupは戻り値をエラーで返すことができる
+	//serverを起動しつつ、contextの終了通知を待機する
+	eg.Go(func() error{
+		if err := s.ListenAndServe(); err != nil && err != http.ErrServerClosed{
+		log.Printf("failed to close server:%v",err)
+		return err
+		}
+		return nil
+	})
+	//キャンセル処理が来たら、shutdownする
+	<-ctx.Done()
+	if err := s.Shutdown(context.Background());err !=nil{
+		log.Printf("failed  to shutdown:%v",err)	
+	}
+
+	return eg.Wait()
 }
