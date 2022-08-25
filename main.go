ba5f4dd79c89c6ca37aea4dc9db4550e393382d2
funasedaisuke <dai.boy.human@gmail.com>
2022-08-26 08:30:59 +0900

    220826:asa syakyo get enviroment

--- main.go
+++ main.go
@@ -1,28 +1,24 @@
 package main
 
 import (
+	"config"
 	"context"
 	"fmt"
 	"log"
 	"net"
 	"net/http"
-	"os"
 
 	"golang.org/x/sync/errgroup"
 )
 
 func main(){
-	if len(os.Args) !=2{
-		log.Printf("need port number\n")
-		os.Exit(1)
+	// if len(os.Args) !=2{
+	// 	log.Printf("need port number\n")
+	// 	os.Exit(1)
 
 
-	}
-	p := os.Args[1]
-	l,err:=net.Listen("tcp",":"+p)
-	if err != nil{
-		log.Fatalf("failed to listen port %s:%v",p,err)
-	}
+	// }
+	// p := os.Args[1]
 
 	//context.Background()は空のコンテキストを生成する。 
 	//コンテキストを使う事で関数の外部からサーバーのプロセスを中断できる
@@ -31,7 +27,19 @@ func main(){
 	}
 }
 
-func run(ctx context.Context,l net.Listener)error{
+func run(ctx context.Context)error{
+	cfg,err := config.New()
+	if err != nil{
+		return err
+
+	}
+	l,err:=net.Listen("tcp",fmt.Sprintf("%d",cfg.Port))
+	if err != nil{
+		log.Fatalf("failed to listen port %d:%v",cfg.Port,err)
+	}
+	url :=fmt.Sprintf("http://%s",l.Addr().String())
+	log.Printf("start with %s",url)
+
 	s := &http.Server{
 		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
 		fmt.Fprintf(w,"hello!:%s",r.URL.Path[1:])
