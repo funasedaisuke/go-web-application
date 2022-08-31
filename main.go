61e159d9ee148e42c5e427c0213d56285ab2fa6b
funasedaisuke <dai.boy.human@gmail.com>
2022-08-31 10:48:22 +0900

    220831:asa syakyo: add tasks entity

--- main.go
+++ main.go
@@ -5,15 +5,9 @@ import (
 	"fmt"
 	"log"
 	"net"
-	"net/http"
 	"os"
-	"os/signal"
-	"syscall"
-	"time"
 
 	"github.com/funasedaisuke/go-web-application/config"
-
-	"golang.org/x/sync/errgroup"
 )
 
 func main(){
@@ -30,12 +24,13 @@ func main(){
 	//コンテキストを使う事で関数の外部からサーバーのプロセスを中断できる
 	if err := run(context.Background());err != nil{
 		log.Printf("failed  to terminate server:%v",err)
+		os.Exit(1)
 	}
 }
 
 func run(ctx context.Context)error{
-	ctx,stop := signal.NotifyContext(ctx,os.Interrupt,syscall.SIGTERM)
-	defer stop()
+	// ctx,stop := signal.NotifyContext(ctx,os.Interrupt,syscall.SIGTERM)
+	// defer stop()
 	cfg,err := config.New()
 	if err != nil{
 		return err
@@ -46,29 +41,32 @@ func run(ctx context.Context)error{
 	}
 	url :=fmt.Sprintf("http://%s",l.Addr().String())
 	log.Printf("start with %s",url)
+	mux := NewMux()
+	s := NewServer(l,mux)
+	return s.Run(ctx)
 
-	s := &http.Server{
-		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
-		time.Sleep(5 * time.Second)
-		fmt.Fprintf(w,"hello!:%s",r.URL.Path[1:])
-	}),
-	}
-	eg,ctx := errgroup.WithContext(ctx)
-	//別ゴールーチンでサーバーを立ち上げる
-	//errgroupは戻り値をエラーで返すことができる
-	//serverを起動しつつ、contextの終了通知を待機する
-	eg.Go(func() error{
-		if err := s.Serve(l); err != nil && err != http.ErrServerClosed{
-		log.Printf("failed to close server:%v",err)
-		return err
-		}
-		return nil
-	})
-	//キャンセル処理が来たら、shutdownする
-	<-ctx.Done()
-	if err := s.Shutdown(context.Background());err !=nil{
-		log.Printf("failed  to shutdown:%v",err)	
-	}
+	// s := &http.Server{
+	// 	Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
+	// 	time.Sleep(5 * time.Second)
+	// 	fmt.Fprintf(w,"hello!:%s",r.URL.Path[1:])
+	// }),
+	// }
+	// eg,ctx := errgroup.WithContext(ctx)
+	// //別ゴールーチンでサーバーを立ち上げる
+	// //errgroupは戻り値をエラーで返すことができる
+	// //serverを起動しつつ、contextの終了通知を待機する
+	// eg.Go(func() error{
+	// 	if err := s.Serve(l); err != nil && err != http.ErrServerClosed{
+	// 	log.Printf("failed to close server:%v",err)
+	// 	return err
+	// 	}
+	// 	return nil
+	// })
+	// //キャンセル処理が来たら、shutdownする
+	// <-ctx.Done()
+	// if err := s.Shutdown(context.Background());err !=nil{
+	// 	log.Printf("failed  to shutdown:%v",err)	
+	// }
 
-	return eg.Wait()
+	// return eg.Wait()
 }
