74520fa7c66eed0cab46aaa19fb327fe5f179072
funasedaisuke <dai.boy.human@gmail.com>
2022-09-15 17:55:08 +0900

    220915:yoru-syakyo:add kvs_func

--- testutil/db.go
+++ testutil/db.go
@@ -1,11 +1,13 @@
 package testutil
 
 import (
+	"context"
 	"database/sql"
 	"fmt"
 	"os"
 	"testing"
 
+	"github.com/go-redis/redis/v8"
 	"github.com/jmoiron/sqlx"
 )
 func OpenDBForTest(t *testing.T) *sqlx.DB{
@@ -26,4 +28,23 @@ func OpenDBForTest(t *testing.T) *sqlx.DB{
 		func() {_ = db.Close()},
 	)
 	return sqlx.NewDb(db,"mysql")
+}
+
+
+func OpenRedisForTest(t *testing.T)*redis.Client{
+	t.Helper()
+	host := "127.0.1"
+	port := 36379
+	if _,defined := os.LookupEnv("CI");defined{
+		port = 6379
+	}
+	client := redis.NewClient(&redis.Options{
+		Addr: fmt.Sprintf("%s:%d",host,port),
+		Password: "",
+		DB: 0,
+	})
+	if err := client.Ping(context.Background()).Err();err != nil{
+		t.Fatalf("failed tp connet redis: %s",err)
+	}
+	return client
 }
